9e9bce3111be238dae3a6516ac4af1e3222faf04
fabiocardosolisboa <fabio.cardosolisboa7@gmail.com>
2021-05-20 16:07:31 -0300

    salvando foto da webcam com janela

---
+++ teste2/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace teste2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbCamera = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.imageList = new System.Windows.Forms.ListView();
+            this.btnImagem = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbCamera
+            // 
+            this.cbCamera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCamera.FormattingEnabled = true;
+            this.cbCamera.Location = new System.Drawing.Point(12, 12);
+            this.cbCamera.Name = "cbCamera";
+            this.cbCamera.Size = new System.Drawing.Size(121, 21);
+            this.cbCamera.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(139, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(220, 10);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
+            this.btnCapture.TabIndex = 2;
+            this.btnCapture.Text = "capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(301, 10);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(405, 10);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(466, 333);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 327);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(236, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(227, 327);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // imageList
+            // 
+            this.imageList.HideSelection = false;
+            this.imageList.Location = new System.Drawing.Point(538, 39);
+            this.imageList.Name = "imageList";
+            this.imageList.Size = new System.Drawing.Size(148, 330);
+            this.imageList.TabIndex = 6;
+            this.imageList.UseCompatibleStateImageBehavior = false;
+            this.imageList.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.imageList_ItemSelectionChanged_1);
+            // 
+            // btnImagem
+            // 
+            this.btnImagem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnImagem.Location = new System.Drawing.Point(611, 10);
+            this.btnImagem.Name = "btnImagem";
+            this.btnImagem.Size = new System.Drawing.Size(75, 23);
+            this.btnImagem.TabIndex = 7;
+            this.btnImagem.Text = "imagens";
+            this.btnImagem.UseVisualStyleBackColor = false;
+            this.btnImagem.Click += new System.EventHandler(this.btnImagem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 384);
+            this.Controls.Add(this.btnImagem);
+            this.Controls.Add(this.imageList);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cbCamera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCamera;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListView imageList;
+        private System.Windows.Forms.Button btnImagem;
+    }
+}
+
---
+++ teste2/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace teste2
+{
+    public partial class Form1 : Form
+    {
+        private List<Image> LoadedImages { get; set; }
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice VideoDevices;
+        public Form1()
+        {
+            InitializeComponent();
+            getallcameralist();
+        }
+        void getallcameralist() 
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Devices in CaptureDevice) 
+            {
+                cbCamera.Items.Add(Devices.Name);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VideoDevices = new VideoCaptureDevice(CaptureDevice[cbCamera.SelectedIndex].MonikerString);
+                VideoDevices.NewFrame += new NewFrameEventHandler(NewVideoFrame);
+                VideoDevices.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void NewVideoFrame(object sender, NewFrameEventArgs eventArgs) 
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "save image as";
+            sfd.Filter = "image files(*.jpg, *.png) | *.jpg; *.png";
+            ImageFormat imageFormat = ImageFormat.Png;
+            if (sfd.ShowDialog() == DialogResult.OK) 
+            {
+                string ext = System.IO.Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        imageFormat = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        imageFormat = ImageFormat.Bmp;
+                        break;
+                }
+                pictureBox2.Image.Save(sfd.FileName,imageFormat);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (VideoDevices.IsRunning == true) 
+            {
+                VideoDevices.Stop();
+            }
+            Application.Exit(null);
+        }
+        private void LoadImagesFromFolder()
+        {
+            string[] arquivos = Directory.GetFiles(@"C:\Users\fabio\Desktop\imagens\");
+            LoadedImages = new List<Image>();
+            foreach (var item in arquivos)
+            {
+                var tempImage = Image.FromFile(item);
+                LoadedImages.Add(tempImage);
+            }
+        }
+        private void imageList_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+        }
+
+        private void btnImagem_Click(object sender, EventArgs e)
+        {
+            imageList.Clear();
+            //carregando imagens da pasta
+            LoadImagesFromFolder();
+
+            //inicializando lista de imagens
+            ImageList images = new ImageList();
+            images.ImageSize = new Size(130, 40);
+
+            foreach (var image in LoadedImages)
+            {
+                images.Images.Add(image);
+            }
+
+            //configurando nossa listview com imageList
+            imageList.LargeImageList = images;
+
+            for (int itemIndex = 1; itemIndex <= LoadedImages.Count; itemIndex++)
+            {
+                imageList.Items.Add(new ListViewItem($"Imagem {itemIndex}", itemIndex - 1));
+            }
+        }
+
+        private void imageList_ItemSelectionChanged_1(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (imageList.SelectedIndices.Count > 0)
+            {
+                var selectedIndex = imageList.SelectedIndices[0];
+                Image selectedImg = LoadedImages[selectedIndex];
+                pictureBox2.Image = selectedImg;
+            }
+        }
+    }
+}
